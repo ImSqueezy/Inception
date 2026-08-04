164aaa3977f6c47c3a0294e5994cae92ce8ad345
ImSqueezy <ouaallaabdelali1@gmail.com>
2026-08-04 18:15:40 +0100

    wordpress WIP

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,32 +1,50 @@
 #!/bin/bash
-
 set -e
 
-mkdir -p  /run/php
+echo "Starting WordPress..."
 
-# Wait for MariaDB to be reachable before doing anything
-until mariadb  -hmariadb -u"${MYSQL_USER}" -p"${MYSQL_PASSWOd}" ping  --silent -e QUIT; do
-    echo "Waiting for MariaDB..."
+chown -R www-data:www-data /var/www/html
+
+if [ ! -f /var/www/html/wp-load.php ]; then
+    echo "Downloading WordPress..."
+
+    su -s /bin/bash www-data -c "
+        wp core download \
+            --path=/var/www/html
+    "
+fi
+
+
+if [ ! -f /var/www/html/wp-config.php ]; then
+    echo "Creating wp-config.php..."
+
+    su -s /bin/bash www-data -c "
+        wp config create \
+            --path=/var/www/html \
+            --dbname=$MYSQL_DATABASE \
+            --dbuser=$MYSQL_USER \
+            --dbpass=$MYSQL_PASSWORD \
+            --dbhost=mariadb
+    "
+fi
+
+echo "Waiting for MariaDB..."
+
+until wp db check
+do
     sleep 2
 done
 
-if [ ! -f /var/www/html/wp-config.php ]; then
-    wp core download --allow-root
+if [ ! wp core is-installed ]; then
+    echo "Installing WordPress..."
 
-    wp config create \
-        --dbname="${MYSQL_DATABASE}" \
-        --dbuser="${MYSQL_USER}" \
-        --dbpass="${MY_PASSWORD}" \
-        --dbhost="mariadb:3306" \
-        --allow-root
-
-    wp core install \
-        --url="${DOMAIN_NAME}" \
-        --title="${WP_TITLE}" \
-        --admin_user="${WP_ADMIN_USER}" \
-        --admin_password="${WP_ADMIN_PASSWORD}" \
-        --admin_email="${WP_ADMIN_EMAIL}" \
-        --allow-root
+        wp core install \
+            --path=/var/www/html \
+            --url=https://$DOMAIN_NAME \
+            --title='$WP_TITLE' \
+            --admin_user='$WP_ADMIN_USER' \
+            --admin_password='$WP_ADMIN_PASSWORD' \
+            --admin_email='$WP_ADMIN_EMAIL'
 fi
 
 exec php-fpm7.4 -F
