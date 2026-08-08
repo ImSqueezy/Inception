cdf465dc49b31b2b72af2940ab376fa49786cdca
ImSqueezy <ouaallaabdelali@gmail.com>
2026-08-07 22:09:14 -0400

    changes on entrypoint.sh (several errors fixes)

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -4,21 +4,21 @@ set -e
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Initializing MariaDB..."
-
-    mariadb-install-db \
-        --user=mysql \
-        --datadir=/var/lib/mysql
-
-    mysqld --user=mysql --bootstrap <<EOF
-	USE mysql;
-	CREATE DATABASE IF NOT EXISTS \`${MDB_DATABASE}\`;
-	CREATE USER IF NOT EXISTS '${MDB_USER}'@'%' IDENTIFIED BY '${MDB_PASSWORD}';
-	GRANT ALL PRIVILEGES ON \`${MDB_DATABASE}\`.* TO '${MDB_USER}'@'%';
-	ALTER USER 'root'@'localhost' IDENTIFIED BY '${MDB_ROOT_PASS}';
-	FLUSH PRIVILEGES;
-EOF
+if [ ! -d /var/lib/mysql/mysql ]; then
+    echo "Initializing database..."
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 fi
 
+mysqld --user=mysql --skip-networking &
+until mysqladmin ping --silent 2>/dev/null; do sleep 1; done
+
+mysql -u root -p"${MDB_ROOT_PASS}" <<EOF
+    CREATE DATABASE IF NOT EXISTS \`${MDB_DATABASE}\`;
+    CREATE USER IF NOT EXISTS '${MDB_USER}'@'%' IDENTIFIED BY '${MDB_PASSWORD}';
+    GRANT ALL PRIVILEGES ON \`${MDB_DATABASE}\`.* TO '${MDB_USER}'@'%';
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MDB_ROOT_PASS}';
+    FLUSH PRIVILEGES;
+EOF
+
+mysqladmin -u root -p"${MDB_ROOT_PASS}" shutdown
 exec mysqld --user=mysql
